ae3dd4a0e18a07686b1c6a81d434a30838d43e58
chenjin <2014060201023@std.uestc.edu.cn>
2021-05-12 21:00:27 +0800

    rm pop_sh files

--- sh_pop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export CUDA_VISIBLE_DEVICES="2"
-
-python main.py -lr 0.001 --sampler 4 --pool_size 200 --sample_size 200 -s 5 -e 300 -b 128 --weighted --log_path 'logs_ml100k';
-python main.py -lr 0.001 --data ml10M --sampler 4 --pool_size 200 --sample_size 200 -s 5 -e 500 -b 4096 --weighted --log_path 'logs_ml10M';
-python main.py -lr 0.001 --data yelp --sampler 4 --pool_size 200 --sample_size 200 -s 5 -e 500 -b 4096 --weighted --log_path 'logs_yelp';
